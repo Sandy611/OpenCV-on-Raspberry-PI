269110b76a4dfb1d6d1d186e15b27b7ea1afa096
Sandeep Chakraborty <pi.raspberry.sandy@gmail.com>
2021-06-15 17:10:57 +0530

    Update test.py

--- test.py
+++ test.py
@@ -6,6 +6,7 @@ if len(sys.argv) < 2:
     print("USAGE: {} img-file".format(sys.argv[0]))
     sys.exit(1)
 
+    
 # Load an color image in grayscale
 img = cv2.imread(sys.argv[1],0)
 cv2.imshow('image',img)
